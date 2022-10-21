406cbad061566b8278d6eb5ddb25d4d7132374f5
Gunjan <gunjan.limbachiya@gmail.com>
2022-10-21 19:48:44 +0530

    Updates

---
+++ precache-manifest.9fe87a9b0f0ecaf4b0169178b3679a20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f273e4ae1f735cd35555c44843ff9477",
+    "url": "/React-Accordion/index.html"
+  },
+  {
+    "revision": "1690816327d223ad59b5",
+    "url": "/React-Accordion/static/css/main.782a5dd6.chunk.css"
+  },
+  {
+    "revision": "d2dff07e10b0f610bdc7",
+    "url": "/React-Accordion/static/js/2.42923868.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Accordion/static/js/2.42923868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1690816327d223ad59b5",
+    "url": "/React-Accordion/static/js/main.1e74214a.chunk.js"
+  },
+  {
+    "revision": "4bb7ea7a67ba0694e51d",
+    "url": "/React-Accordion/static/js/runtime-main.b5c6d1f7.js"
+  }
+]);
